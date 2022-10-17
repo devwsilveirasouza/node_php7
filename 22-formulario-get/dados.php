58a59678566b3cf6e0469cacb4060067d7004137
devwsilveirasouza <dev.wsilveirasouza@gmail.com>
2022-10-17 19:01:11 -0300

    GET & POST

--- 22-formulario-get/dados.php
+++ 22-formulario-get/dados.php
@@ -6,5 +6,9 @@
     echo "<br>";
     echo "Seu nome é $nome e seu email é $email.";
     echo "<br>";
+    echo $_GET['idade'];
+    echo "<br>";
+    echo $_GET['sobrenome'];
+    echo "<br>";
     var_dump($_GET);
     echo "<br>";
--- 22-formulario-get/index.php
+++ 22-formulario-get/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
 </head>
+
 <body>
 
     <form action="dados.php" method="GET">
@@ -13,9 +15,12 @@
         Nome: <input type="text" name="nome" /><br><br>
         Email: <input type="email" name="email" /><br><br>
 
-        <input type="submit" name="enviar" />
+        <button type="submit" name="enviar">Enviar</button><br>
+        <!-- Enviando parâmetros pela url -->
+        <a href="dados.php?idade=25&sobrenome=Souza">Enviar dados</a>
 
     </form>
 
 </body>
+
 </html>
